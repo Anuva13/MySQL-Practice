0bd25e85f65094731e450710d1969525384d2a1f
Anuva13 <anuvadas@hotmail.com>
2025-03-14 15:42:56 -0400

    Added few more queries

--- Queries/advanced_queries.sql
+++ Queries/advanced_queries.sql
@@ -11,3 +11,29 @@
     join titles as t on e.emp_no = t.emp_no
     where
     e.emp_no in (select emp_no from titles group by emp_no having count(title)>1)
+
+/* Find the employees with second highest and third highest pay */
+    select concat(e.first_name, " ", e.last_name) as emp_name, s.salary
+    from employees as e
+    join salaries as s on s.emp_no = e.emp_no
+    where
+    s.salary < (select salary from salaries order by salary desc limit 1)
+    order by s.salary desc limit 2
+    
+/* Find the employees with the same name */
+    select e.first_name, e.last_name, count(*) as name_count
+    from employees as e
+    group by e.first_name, e.last_name
+    having name_count>0
+    order by name_count desc
+    
+/* Find the employees and their manager names */
+    select 
+    e.emp_no AS employee_id,
+    e.first_name AS employee_name,
+    m.emp_no AS manager_id,
+    m.first_name AS manager_name
+    from employees e
+    join dept_emp de ON e.emp_no = de.emp_no
+    join dept_manager dm ON de.dept_no = dm.dept_no
+    join employees m ON dm.emp_no = m.emp_no
